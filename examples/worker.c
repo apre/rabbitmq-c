21148c94aed9e55f0e8fb8dbb6d24e8e24654a3e
apre <apre@users.noreply.github.com>
2017-01-04 23:39:59 +0100

    reindent example/worker.c

--- examples/worker.c
+++ examples/worker.c
@@ -21,7 +21,7 @@ as the name is changed.
 /**
   \file
   \brief rabbitmq Tutorial 2 (worker)
-  
+
 Tutorial 1 receive using <i>rabbitmq-c</i> library available at
 https://github.com/alanxz/rabbitmq-c
 */
@@ -89,51 +89,43 @@ int main(int argc, char const *const *argv)
 
 
 
-     amqp_basic_consume(conn, 1, amqp_cstring_bytes(queueName), amqp_empty_bytes, 0, 0,0, amqp_empty_table);
-     r = amqp_get_rpc_reply(conn);
-     assert(r.reply_type==AMQP_RESPONSE_NORMAL);
+    amqp_basic_consume(conn, 1, amqp_cstring_bytes(queueName), amqp_empty_bytes, 0, 0,0, amqp_empty_table);
+    r = amqp_get_rpc_reply(conn);
+    assert(r.reply_type==AMQP_RESPONSE_NORMAL);
 
 
-     {
-       for (;;) {
-         amqp_rpc_reply_t res;
-         amqp_envelope_t envelope;
+    {
+        for (;;) {
+            amqp_rpc_reply_t res;
+            amqp_envelope_t envelope;
 
-         amqp_maybe_release_buffers(conn);
+            amqp_maybe_release_buffers(conn);
 
-         res = amqp_consume_message(conn, &envelope, NULL, 0);
+            res = amqp_consume_message(conn, &envelope, NULL, 0);
 
-         if (AMQP_RESPONSE_NORMAL != res.reply_type) {
-           break;
-         }
+            if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+                break;
+            }
 
-         printf("Delivery %u, exchange %.*s routingkey %.*s\n",
-                (unsigned) envelope.delivery_tag,
-                (int) envelope.exchange.len, (char *) envelope.exchange.bytes,
-                (int) envelope.routing_key.len, (char *) envelope.routing_key.bytes);
+            printf("Delivery %u, exchange %.*s routingkey %.*s\n",
+                   (unsigned) envelope.delivery_tag,
+                   (int) envelope.exchange.len, (char *) envelope.exchange.bytes,
+                   (int) envelope.routing_key.len, (char *) envelope.routing_key.bytes);
 
-         if (envelope.message.properties._flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
-           printf("Content-type: %.*s\n",
-                  (int) envelope.message.properties.content_type.len,
-                  (char *) envelope.message.properties.content_type.bytes);
-         }
-         printf("----\n");
+            if (envelope.message.properties._flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
+                printf("Content-type: %.*s\n",
+                       (int) envelope.message.properties.content_type.len,
+                       (char *) envelope.message.properties.content_type.bytes);
+            }
+            printf("----\n");
 
-         amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
-         /*
-         int amqp_basic_ack	(	amqp_connection_state_t 	state,
-         amqp_channel_t 	channel,
-         uint64_t 	delivery_tag,
-         amqp_boolean_t 	multiple
-         )	*/
-         int ack_res = amqp_basic_ack (conn,channel,envelope.delivery_tag,0);
-         assert (ack_res == 0);
-         amqp_destroy_envelope(&envelope);
-       }
-     }
+            amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
 
-
-
+            int ack_res = amqp_basic_ack (conn,channel,envelope.delivery_tag,0);
+            assert (ack_res == 0);
+            amqp_destroy_envelope(&envelope);
+        }
+    }
 
     r=amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
 
